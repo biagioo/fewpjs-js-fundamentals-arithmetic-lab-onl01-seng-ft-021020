dcee94399e95ceff2f71e7d512fc4c41527c42ed
biagioo <mendolia2328@gmail.com>
2020-05-23 08:58:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+
+let multiply = num1 * num2
+
+let random = Math.random()
+
+let mod = 16 % 12
+
+let max = Math.max(1,2,3,4,5,6,6,7,20)
